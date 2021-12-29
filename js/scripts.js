8996d52cc901cbbc17d9d28c9c20be633d13d5fc
tndfame <fame.ngrr@gmail.com>
2021-12-29 19:52:04 +0700

    add file css

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+window.addEventListener('DOMContentLoaded', event => {
+    const listHoursArray = document.body.querySelectorAll('.list-hours li');
+    listHoursArray[new Date().getDay()].classList.add(('today'));
+})
